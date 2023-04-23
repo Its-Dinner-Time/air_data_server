c6d3212313f0fb9a9707265d02d1e6da22e3d7b6
dinnerTime <dinnerTime.dev@gmail.com>
2023-04-23 18:18:29 +0900

    heroku setting

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
 import { IEnvironmentConfigs } from './configs/environments.config';
-import {
-  BadRequestException,
-  ValidationError,
-  ValidationPipe,
-} from '@nestjs/common';
+import { BadRequestException, ValidationError, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,13 +17,13 @@ async function bootstrap() {
       // custom vaildation error filter
       exceptionFactory: (validationErrors: ValidationError[] = []) => {
         const error = validationErrors[0];
-        const message = `${error.property}: ${
-          Object.values(error.constraints)[0]
-        }`;
+        const message = `${error.property}: ${Object.values(error.constraints)[0]}`;
         return new BadRequestException(message, { description: 'DTO ERROR' });
       },
     }),
   );
+
+  app.enableCors();
 
   const configService = app.get(ConfigService<IEnvironmentConfigs>);
   const PORT = configService.getOrThrow('port');
